28bb3cba4c586592a689243be7a57ba4e724eb7f
patshuff <pat@patshuff.com>
2020-10-23 10:28:26 -0500

    Create variables.tf

---
+++ vsphere/minimal/variables.tf
@@ -0,0 +1,14 @@
+
+variable "vsphere_user" {
+  type    = string
+  default = "administrator@patshuff.com"
+}
+
+variable "vsphere_password" {
+  type = string
+}
+
+variable "vsphere_server" {
+  type    = string
+  default = "10.0.0.93"
+}
