525878d7cda9cc57d89522968cf9b02aba6d1462
patshuff <pat@patshuff.com>
2020-10-27 14:44:21 -0500

    update to connect powershell script

--- vsphere/minimal/connect.ps1
+++ vsphere/minimal/connect.ps1
@@ -1,5 +1,24 @@
 
+# This program connects to a vSphere server and generates a variables.tf file that can be used as a basis for a terraform build
+# 
+# the program starts by looking for TF_VAR_vsphere_server, TF_VAR_vsphere_user, and TF_VAR_vsphere_password environment variables
+#  and pushing them into the variables file if they are not defined and leaving them blank if they are defined.
+#
+# the program then connects to the vSphere server and creates a terraform ready file that can be used for vsphere provider, vsphere_hosts, 
+#  vsphere_datastore, vsphere_resource_pool, and virtual machine templates. 
+#
+# assumptions:
+#  username and password to connect to a vSphere server using the PowerCLI tools installed in a PowerShell 6+ environment. 
+#
+
+# change file_name to variables.tf if you want a single step generation of the variables.tf file. This program will prompt overwrite but will not
+#   preserve the old file.
+#
+# Created and maintained by: pat@patshuff.com
+#
 $file_name = "test.yy"
+
+# test to verify overwriting $file_name is ok and exit if it is not
 if (Test-Path $file_name) {
   $q1 = 'overwrite ' + $file_name + '? (type yes to confirm)'
   $resp = Read-Host -Prompt $q1
@@ -8,7 +27,14 @@ if (Test-Path $file_name) {
 	Exit
   }
 }  
+
+# Start-Transcript is used to record the output of this script and generate the $file_name file
 Start-Transcript -UseMinimalHeader -Path "$file_name"
+
+Write-Host "# this file is auto-generated from connect.ps1 file, edit at your own risk"
+Write-Host "# the server, username, and password should be defined in environment variable or .tfvars file"
+
+# check for a vsphere_server definition in environment variables
 if (!$TF_VAR_vsphere_server) {
   $TF_VAR_vsphere_server = Read-Host -Prompt 'Input your server name'
   Write-Host -Separator "" 'variable "vsphere_server" {
@@ -20,7 +46,9 @@ if (!$TF_VAR_vsphere_server) {
     type    = string 
   }'
 }
+Write-Host " "
 
+# check for a vsphere_user definition in environment variables
 if (!$TF_VAR_vsphere_user) {
   $TF_VAR_vsphere_user = Read-Host -Prompt 'Connect with username'
   Write-Host -Separator "" 'variable "vsphere_user" {
@@ -32,9 +60,11 @@ if (!$TF_VAR_vsphere_user) {
     type    = string
   }'
 }
+Write-Host " "
 
+# check for a vsphere_password definition in environment variables
 if (!$TF_VAR_vsphere_password) {
-  $TF_VAR_vsphere_password = Read-Host -Prompt 'Connect with username'
+  $TF_VAR_vsphere_password = Read-Host -Prompt 'Connect with password'
   Write-Host -Separator "" 'variable "vsphere_password" {
     type    = string
     default = "'$TF_VAR_vsphere_password'"'
@@ -44,9 +74,23 @@ if (!$TF_VAR_vsphere_password) {
     type    = string
   }'
 }
+Write-Host " "
 
+Write-Host "# provider vsphere declaration"
+Write-Host " "
+# connect to the vSphere server
 $connect = Connect-VIServer -Server $TF_VAR_vsphere_server -User $TF_VAR_vsphere_user -Password $TF_VAR_vsphere_password
 
+# output provider vsphere declaration
+Write-Host 'provider "vsphere" {
+  user = var.vsphere_user
+  password = var.vsphere_password
+  vsphere_server = var.vsphere_server
+  allow_unverified_ssl = true
+}'
+Write-Host " "
+
+# output vsphere_datacenter declaration
 $dc = Get-Datacenter
 Write-Host '# vsphere_datacenter definition'
 Write-Host ' '
@@ -55,13 +99,7 @@ Write-Host -Separator "" 'data "vsphere_datacenter" "dc" {
    '}'
 Write-Host ' '   
 
-Write-Host 'provider "vsphere" {
-  user = var.vsphere_user
-  password = var.vsphere_password
-  vsphere_server = var.vsphere_server
-  allow_unverified_ssl = true
-}'
-
+# search for templates and create a vsphere_virtual_machine for each template found
 Write-Host '# vsphere_virtual_machine (template) definition'
 Write-Host ' '
 $Template_Name = @()
@@ -77,6 +115,7 @@ Write-Host ' '
 }
 Write-Host ' '   
 
+# search for hosts and create a vsphere_host for each host found
 Write-Host '# vsphere_host definition'
 Write-Host ' '
 $Host_name = @()
@@ -97,24 +136,36 @@ foreach ($item in $Host_name) {
 }
 Write-Host ' '   
 
+# search for datastores and create a vsphere_datastore for each host found
 Write-Host '# vsphere_datastore definition'
 Write-Host ' '
 $Datastore_name = @()
 $Datastore_name = Get-Datastore
 
 foreach ($item in $Datastore_name) {
+# get rid of spaces, open and close parenthesis, and postfix ds if datastore starts with non character
   $item2 = $item -replace ' ',''
   $item2 = $item2 -replace '\(','_'
   $item2 = $item2 -replace '\)',''
   $item2 = $item2 -replace '\.','_'
-  Write-Host -Separator "" 'data "vsphere_datastore" "ds-'$item2'"' ' {
+  # if datastore starts with non charater, postfix ds- before name
+  if ($item2 -match '^[a-z]') {
+  Write-Host -Separator "" 'data "vsphere_datastore" "'$item2'"' ' {
    name = "'$item'"'
    '   datacenter_id = data.vsphere_datacenter.dc.id
 }'
+   }
+   else {
+   Write-Host -Separator "" 'data "vsphere_datastore" "ds-'$item2'"' ' {
+   name = "'$item'"'
+   '   datacenter_id = data.vsphere_datacenter.dc.id
+}'
+   }
 Write-Host ' '
-}
+} 
 Write-Host ' '   
 
+# search for network connections and create a vsphere_network for each host found
 Write-Host '# vsphere_network definition'
 Write-Host ' '
 $Network_name = @()
@@ -130,6 +181,7 @@ Write-Host ' '
 }
 Write-Host ' '   
 
+# output the variables.tf file to $file_name and truncate the first few and last few lines residual from the Transcript command
 Stop-Transcript
 $test = Get-Content "$file_name"
 $test[5..($test.count - 5)] | Out-File "$file_name"
--- vsphere/minimal/variables.tf
+++ vsphere/minimal/variables.tf
@@ -1,17 +1,18 @@
+# this file is auto-generated from connect.ps1 file, edit at your own risk
+# the server, username, and password should be defined in environment variable or .tfvars file
 variable "vsphere_server" { 
     type    = string 
   }
+
 variable "vsphere_user" {
     type    = string
   }
+
 variable "vsphere_password" {
     type    = string
   }
-# vsphere_datacenter definition
 
-data "vsphere_datacenter" "dc" {
-   name = "Home-lab"
-}
+# provider vsphere declaration
 
 provider "vsphere" {
   user = var.vsphere_user
@@ -19,15 +20,22 @@ provider "vsphere" {
   vsphere_server = var.vsphere_server
   allow_unverified_ssl = true
 }
-# vsphere_virtual_machine (template) definition
 
-data "vsphere_virtual_machine" "win_10_template" {
-   name = "win_10_template"
-   datacenter_id = data.vsphere_datacenter.dc.id
+# vsphere_datacenter definition
+
+data "vsphere_datacenter" "dc" {
+   name = "Home-lab"
 }
+
+# vsphere_virtual_machine (template) definition
 
 data "vsphere_virtual_machine" "win-2019-template" {
    name = "win-2019-template"
+   datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "win_10_template" {
+   name = "win_10_template"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
@@ -54,16 +62,6 @@ data "vsphere_resource_pool" "Resources-10_0_0_3" {
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_host" "Host-10_0_0_71" {
-   name = "10.0.0.71"
-   datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_resource_pool" "Resources-10_0_0_71" {
-   name = "10.0.0.71/Resources"
-   datacenter_id = data.vsphere_datacenter.dc.id
-}
-
 data "vsphere_host" "Host-10_0_0_94" {
    name = "10.0.0.94"
    datacenter_id = data.vsphere_datacenter.dc.id
@@ -74,45 +72,55 @@ data "vsphere_resource_pool" "Resources-10_0_0_94" {
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_host" "Host-10_0_0_71" {
+   name = "10.0.0.71"
+   datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "Resources-10_0_0_71" {
+   name = "10.0.0.71/Resources"
+   datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 
 # vsphere_datastore definition
 
-data "vsphere_datastore" "ds-datastore1" {
+data "vsphere_datastore" "datastore1" {
    name = "datastore1"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-USB-DS3_2" {
+data "vsphere_datastore" "USB-DS3_2" {
    name = "USB-DS3.2"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-ds2-ssd" {
+data "vsphere_datastore" "ds2-ssd" {
    name = "ds2-ssd"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-datastore1_2" {
+data "vsphere_datastore" "datastore1_2" {
    name = "datastore1 (2)"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-ds0_5" {
+data "vsphere_datastore" "ds0_5" {
    name = "ds0.5"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-ds3-ssd" {
+data "vsphere_datastore" "ds3-ssd" {
    name = "ds3-ssd"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-external" {
+data "vsphere_datastore" "external" {
    name = "external"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-ds1_8t" {
+data "vsphere_datastore" "ds1_8t" {
    name = "ds1.8t"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
@@ -122,22 +130,22 @@ data "vsphere_datastore" "ds-235NFSShare" {
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-datastore1_1" {
+data "vsphere_datastore" "datastore1_1" {
    name = "datastore1 (1)"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-USB-DS2" {
+data "vsphere_datastore" "USB-DS2" {
    name = "USB-DS2"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-datastore1-ssd" {
+data "vsphere_datastore" "datastore1-ssd" {
    name = "datastore1-ssd"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_datastore" "ds-datastore2" {
+data "vsphere_datastore" "datastore2" {
    name = "datastore2"
    datacenter_id = data.vsphere_datacenter.dc.id
 }
