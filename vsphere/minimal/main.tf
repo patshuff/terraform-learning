3cdae1746475700082fc043b9ad80cf681ca94bd
patshuff <pat@patshuff.com>
2020-10-27 15:16:51 -0500

    Update main.tf

--- vsphere/minimal/main.tf
+++ vsphere/minimal/main.tf
@@ -8,7 +8,7 @@ resource "vsphere_virtual_machine" "vm" {
    name = "terraform-test"
    resource_pool_id = data.vsphere_resource_pool.Resources-10_0_0_92.id
    host_system_id = data.vsphere_host.Host-10_0_0_92.id
-   guest_id = "windows9_64Guest"
+   guest_id = data.vsphere_virtual_machine.win_10_template.guest_id
    network_interface {
      network_id = data.vsphere_network.VMNetwork.id
    }
