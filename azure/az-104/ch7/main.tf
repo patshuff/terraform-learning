5acdb36e4e97fc5d9409abf33d6e427596001817
patshuff <pat@patshuff.com>
2020-11-20 08:54:57 -0600

    Create main.tf

---
+++ azure/az-104/ch7/main.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "Simple_Example_Resource_Group"
+  location = "westus"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualNetwork1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "example" {
+  name                 = "testsubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
